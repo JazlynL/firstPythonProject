c677fbcb85d2b9ba3a22a3079bcb843f4da7f9f6
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-28 00:26:43 -0400

    Finishing The Student Checker. Completed.

--- areyoustudent.py
+++ areyoustudent.py
@@ -1,37 +1,37 @@
 STUDENTS = ["Becky", "Jake", "Cameron", "Jessie", "Karol", "Frankie"]
 SCHOOL_NAME = "Career Devs"
 
-# challenge 1
-# accept = input("Hello are you a student at " + SCHOOL_NAME + "? \n (yes/no)")
-# if accept == "yes":
-#     name = input("Okay What is your Name ? \n Name:")
-#     for stu_name in STUDENTS:
-#         if name == stu_name:
-#             print("Welcome to class")
-#
-#             break
-#
-#
-# else:
-#     print("You are not supposed to be here.")
+#challenge 1
+accept = input("Hello are you a student at " + SCHOOL_NAME + "? \n (yes/no)")
+if accept == "yes":
+    name = input("Okay What is your Name ? \n Name:")
+    for stu_name in STUDENTS:
+        if name == stu_name:
+            print("Welcome to class")
+
+            break
 
 
-# Bonus challenges
-# Ignore the whitespace of the user inputs
-# allow all inputs that start with ‘y’ to be a valid ‘yes’
-# allow a user to re-enter if they don't enter yes or no
+else:
+    print("You are not supposed to be here.")
 
-# accept = input("Hello are you a student at " + SCHOOL_NAME + "? \n (yes/no)").strip()
-# if accept[0] == "y":
-#     name = input("Okay What is your Name ? \n Name:")
-#     for stu_name in STUDENTS:
-#         if name == stu_name:
-#             print("Welcome to class")
-#
-#             break
-#
-# else:
-#     print("You are not supposed to be here.")
+
+#Bonus challenges
+#Ignore the whitespace of the user inputs
+#allow all inputs that start with ‘y’ to be a valid ‘yes’
+#allow a user to re-enter if they don't enter yes or no
+
+accept = input("Hello are you a student at " + SCHOOL_NAME + "? \n (yes/no)").strip()
+if accept[0] == "y":
+    name = input("Okay What is your Name ? \n Name:")
+    for stu_name in STUDENTS:
+        if name == stu_name:
+            print("Welcome to class")
+
+            break
+
+else:
+    print("You are not supposed to be here.")
 
 accept = " "
 while accept == "" or accept[0] != "y" and accept[0] != "n":
