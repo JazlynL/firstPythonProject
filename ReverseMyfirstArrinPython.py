9c1ee2860a258825c8b70f0c8cc1f73aae9d779c
“JazlynL” <lubenjazlyn35@gmail.com>
2022-05-16 18:54:22 -0400

    reversing Array in python

---
+++ ReverseMyfirstArrinPython.py
@@ -0,0 +1,8 @@
+def reverse(lst):
+    # A list is a data structure in Python that is a mutable,
+    # or changeable, ordered sequence of elements. Each element
+    # or value that is inside of a list is called an item.
+    # Just as strings are defined as characters between quotes,
+    # lists are defined by having values between square brackets [ ]
+    array = list(reversed(lst))
+    return array
